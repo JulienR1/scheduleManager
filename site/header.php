1cfdd25539871cc684556afbc22ec66de207c625
Julien <julrousseau20@gmail.com>
2020-05-28 00:26:52 -0400

    Base layout

---
+++ site/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Ferme les Vieilles Forges</title>
+
+    <link rel="stylesheet" href="css/header/header.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Ferme les Vieilles Forges</h1>
+
+        <figure>
+            <img src="#" alt="Face picture">
+            <figcaption>
+                <div id="name">
+                    Karol<b>Destroismaisons</b>
+                </div>
+                <form action="" method="post">
+                    <button type="submit" name="logout-submit">Déconnexion</button>
+                </form>
+                <button id="login-button">Se connecter</button>
+            </figcaption>
+        </figure>
+
+        <nav>
+            <ul>
+                <li><a href="#">Horaire</a></li>
+                <li><a href="#">Mes dispos</a></li>
+                <li><a href="#">Mes heures</a></li>
+            </ul>
+        </nav>
+    </header>
