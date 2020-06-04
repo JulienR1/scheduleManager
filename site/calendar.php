68e1ef46aea252ae829265c624b95860485dd200
Julien <julrousseau20@gmail.com>
2020-06-03 21:17:29 -0400

    Calendrier cellulaire

--- site/calendar.php
+++ site/calendar.php
@@ -11,8 +11,8 @@ require "req/header.php";
             <button id="month">Novembre</button>
             <?php require "req/header-toggle.php"?>
         </h3>
-        <div id="container">
-            <table>
+        <table>
+            <tbody>
                 <tr>
                     <th>D</th>
                     <th>L</th>
@@ -50,8 +50,100 @@ require "req/header.php";
                         <h4>5</h4>
                     </td>
                 </tr>
-            </table>
-        </div>
+                <tr>
+                    <td>
+                        <h4>6</h4>
+                    </td>
+                    <td>
+                        <h4>7</h4>
+                    </td>
+                    <td>
+                        <h4>8</h4>
+                    </td>
+                    <td>
+                        <h4>9</h4>
+                    </td>
+                    <td>
+                        <h4>10</h4>
+                    </td>
+                    <td>
+                        <h4>11</h4>
+                    </td>
+                    <td>
+                        <h4>12</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>12</h4>
+                    </td>
+                    <td>
+                        <h4>13</h4>
+                    </td>
+                    <td>
+                        <h4>14</h4>
+                    </td>
+                    <td>
+                        <h4>15</h4>
+                    </td>
+                    <td>
+                        <h4>16</h4>
+                    </td>
+                    <td>
+                        <h4>17</h4>
+                    </td>
+                    <td>
+                        <h4>18</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>18</h4>
+                    </td>
+                    <td>
+                        <h4>20</h4>
+                    </td>
+                    <td>
+                        <h4>21</h4>
+                    </td>
+                    <td>
+                        <h4>22</h4>
+                    </td>
+                    <td>
+                        <h4>23</h4>
+                    </td>
+                    <td>
+                        <h4>24</h4>
+                    </td>
+                    <td>
+                        <h4>25</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>26</h4>
+                    </td>
+                    <td>
+                        <h4>27</h4>
+                    </td>
+                    <td>
+                        <h4>28</h4>
+                    </td>
+                    <td>
+                        <h4>29</h4>
+                    </td>
+                    <td>
+                        <h4>30</h4>
+                    </td>
+                    <td>
+                        <h4>31</h4>
+                    </td>
+                    <td>
+                        <h4>1</h4>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </section>
 
     <button id="right"><i class="fas fa-caret-right"></i></button>
