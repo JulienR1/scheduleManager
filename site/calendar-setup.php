a9d844e0b03c1dc479ca80a00504eccf129544f0
Julien <julrousseau20@gmail.com>
2020-06-17 20:35:16 -0400

    Style creation horaire desktop

--- site/calendar-setup.php
+++ site/calendar-setup.php
@@ -8,8 +8,8 @@ if (!isset($_SESSION["userID"]) || !$_SESSION["isAdmin"]) {
 
 <main>
     <form id="calendar-setup">
-        <h3>
-            <div class="container shadow-bg">
+        <h3 class="shadow-bg-desktop">
+            <div class="container shadow-bg-cell">
                 <div id="month-selection">
                     <label for="month-dropdown">Mois: </label>
                     <select type="submit" id="month-dropdown">
@@ -24,7 +24,7 @@ for ($i = 0; $i < sizeof($months); $i++) {
                 <?php require "req/header-toggle.php"?>
             </div>
             <div class="container">
-                <div id="weekday-selection" class="shadow-bg">
+                <div id="weekday-selection" class="shadow-bg-cell">
                     <label for="weekday-dropdown">Semaine du: </label>
                     <input type="text" id="weekday-dropdown">
                 </div>
@@ -63,8 +63,35 @@ for ($i = 0; $i < sizeof($months); $i++) {
                 </div>
             </div>
 
-            <div class="day shadow-bg" closed>
+            <div class="day shadow-bg">
                 <h4 onclick="toggleDayView()" class="shadow-bg">Lundi</h4>
+                <div class="wrapper">
+                    <div class="task">
+                        <button onclick="removeTask()" type="button" id="cancel"><i class="fas fa-times"></i></button>
+                        <div class="time">
+                            <input type="text" class="startTime" placeholder="0:00">
+                            <p>-</p>
+                            <input type="text" class="endTime" placeholder="0:00">
+                        </div>
+                        <div class="title">
+                            <select name="taskName" id="taskName">
+                                <option value="Asperges">Asperges</option>
+                                <option value="Fleur de fraises">Fleurs de fraises</option>
+                            </select>
+                            <div id="qty">
+                                <span>(</span>
+                                <input type="text" placeholder="Qté">
+                                <span>)</span>
+                            </div>
+                        </div>
+                        <ul>
+                            <li><select name="user1" class="userSelection">
+                                    <option value="user1">Filip</option>
+                                </select></li>
+                            <li><select name="user1" class="userSelection"></select></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
             <div class="day shadow-bg" closed>
