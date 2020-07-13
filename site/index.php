37e1ce1746921822afc3196b0748020e0fe731ab
Julien Rousseau <julien.rousseau@augergroupe.com>
2020-07-13 00:12:54 -0400

    html accueil

--- site/index.php
+++ site/index.php
@@ -4,6 +4,13 @@ require "req/header.php";
 
 <main>
     <?php require "req/header-toggle.php";?>
+
+    <div id="lazy-container">
+        <p>Oui je suis trop lazy pour faire une belle page d'accueil</p>
+        <p>Il suffit de se connecter pour avoir accès aux horaires</p>
+
+        <a href="calendar.php">L'horaire ici</a>
+    </div>
 </main>
 
 <?php
